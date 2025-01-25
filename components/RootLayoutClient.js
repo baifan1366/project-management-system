1191f51fb31df80a87d61a0461a49703354765c9
baifan1366 <baifan1366@gmail.com>
2025-01-25 16:52:17 +0800

    feat: add pricing header component

--- components/RootLayoutClient.js
+++ components/RootLayoutClient.js
@@ -3,13 +3,18 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { Providers } from '@/app/providers';
 import { Header } from './Header';
+import { PricingHeader } from './PricingHeader';
+import { usePathname } from 'next/navigation';
 
 export function RootLayoutClient({ children, locale, messages }) {
+  const pathname = usePathname();
+  const isPricingPage = pathname.includes('/pricing');
+
   return (
     <NextIntlClientProvider locale={locale} messages={messages} timeZone="Asia/Shanghai">
       <Providers>
         <div className="relative flex min-h-screen flex-col">
-          <Header />
+          {isPricingPage ? <PricingHeader /> : <Header />}
           <main className="flex-1 container py-6">
             {children}
           </main>
